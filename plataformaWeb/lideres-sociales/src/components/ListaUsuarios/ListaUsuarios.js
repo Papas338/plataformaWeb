a00c103621f2dc81a8159004ed463f3fc4e24474
julian9809 <juliand9809@gmail.com>
2020-10-07 17:43:16 -0500

    avance en aprobar usuarios

--- plataformaWeb/lideres-sociales/src/components/ListaUsuarios/ListaUsuarios.js
+++ plataformaWeb/lideres-sociales/src/components/ListaUsuarios/ListaUsuarios.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { map, isEmpty } from "lodash";
 import User from "./User";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExclamationTriangle } from "@fortawesome/free-solid-svg-icons";
 
 import "./ListaUsuarios.scss";
 
@@ -8,7 +10,12 @@ export default function (props) {
   const { users } = props;
 
   if (isEmpty(users)) {
-    return <h2>No se encuentran usuarios registrados</h2>;
+    return (
+      <div className="error-usuarios">
+        <FontAwesomeIcon icon={faExclamationTriangle} />
+        <h2>No se encuentran usuarios registrados</h2>
+      </div>
+    );
   }
 
   return (
