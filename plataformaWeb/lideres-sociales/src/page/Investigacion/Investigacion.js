a6dea2615e0c7caf74b25643c6c7e7406ec448de
julian9809 <juliand9809@gmail.com>
2020-12-11 18:03:58 -0500

    descar de entrevista

--- plataformaWeb/lideres-sociales/src/page/Investigacion/Investigacion.js
+++ plataformaWeb/lideres-sociales/src/page/Investigacion/Investigacion.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Container, Row, Col, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { logoutApi } from "../../api/auth";
-import investigacion from "../../document/Documento final proyecto de investigacion Lideres Sociales.pdf"
+import investigacion from "../../document/Documento final proyecto de investigacion Lideres Sociales.pdf";
 
 export default function Investigacion(props) {
 
@@ -16,11 +16,17 @@ export default function Investigacion(props) {
     return (
         <>
             <Row>
-                <Col className="visuaLider__header" sm={8}>
+                <Col className="visuaLider__header" sm={6}>
                     <h2>Ver Investigación</h2>
                 </Col>
                 <Col className="visuaLider__headerIndex" sm={2}>
-                    <Button variant="success" as={Link} to="/">Volver al inicio</Button>
+                    {/* <Button variant="info" as={Link} to="/">Volver al inicio</Button> */}
+                    <Button variant="info" as="a" href={require('../../files/interviews/Entrevista unida Liney.mp3')} download="Entrevista Liney.mp3">
+                        Descargar entrevista
+                    </Button>
+                </Col>
+                <Col className="visuaLider__headerIndex" sm={2}>
+                    <Button variant="success" as={Link} to="/">Volver al inicio</Button>                    
                 </Col>
                 <Col className="visuaLider__headerLogout" sm={2}>
                     <Button as={Link} variant="danger" onClick={logout}>Cerrar Sesión</Button>
